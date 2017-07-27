4fd0e2691a9b95714f819ed4731c625ef58e11f3
Lucian Smith <luxianosu@gmail.com>
2017-07-27 14:35:04 -0600

    Setting up server

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,29 @@
-//jsjsk
+var config = {
+   entry: './main.js',
+
+   output: {
+      path:'./',
+      filename: 'index.js',
+   },
+
+   devServer: {
+      inline: true,
+      port: 8080
+   },
+
+   module: {
+      loaders: [
+         {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel',
+
+            query: {
+               presets: ['es2015', 'react']
+            }
+         }
+      ]
+   }
+}
+
+module.exports = config;
